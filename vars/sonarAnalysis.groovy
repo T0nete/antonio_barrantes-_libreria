b7674a9fa77f7b35c5aacbfc0c73c7aae61923a1
T0nete <toni.barrantes.montilla64@gmail.com>
2024-03-16 19:24:45 +0100

    add version

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,15 +1,17 @@
 def call(boolean abortPipeline = false, boolean sonarExecution = true) {
-    timeout(time: 5, unit: 'MINUTES') {
-        if (sonarExecution) {
-            sh 'echo "Ejecución de las pruebas de calidad de código"'
-        } else {
-            echo 'Simulando ejecución de SonarQube'
+    withSonarQubeEnv('SonarQube') {
+        timeout(time: 5, unit: 'MINUTES') {
+            if (sonarExecution) {
+                sh 'echo "Ejecución de las pruebas de calidad de código"'
+            } else {
+                echo 'Simulando ejecución de SonarQube'
+            }
         }
-    }
 
-    if (abortPipeline) {
-        echo 'Evaluando QualityGate de SonarQube...'
-    } else {
-        echo 'Continuando el pipeline...'
+        if (abortPipeline) {
+            echo 'Evaluando QualityGate de SonarQube...'
+        } else {
+            echo 'Continuando el pipeline...'
+        }
     }
 }
