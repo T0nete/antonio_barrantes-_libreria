97700408726f0edc928f3957684c274c2f7810c3
T0nete <toni.barrantes.montilla64@gmail.com>
2024-03-24 18:06:03 +0100

    remove default values

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,11 +1,13 @@
-def call(boolean abortPipeline = false, boolean sonarExecution = true) {
+def call(boolean abortPipeline, boolean sonarExecution) {
+    def abort = abortPipeline ?: false
+    def sonar = sonarExecution ?: false
     def branch = env.BRANCH_NAME
     
-    def shouldAbort = abortPipeline || (branch == 'master') || (branch.startsWith('hotfix'))
+    def shouldAbort = abort || (branch == 'master') || (branch.startsWith('hotfix'))
 
     withSonarQubeEnv('Sonar Local') {
         timeout(time: 5, unit: 'MINUTES') {
-            if (sonarExecution) {
+            if (sonar) {
                 sh 'echo "Ejecución de las pruebas de calidad de código"'
             } else {
                 echo 'Simulando ejecución de SonarQube'
