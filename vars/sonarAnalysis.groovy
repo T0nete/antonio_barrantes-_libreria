9b9160d505ae95f787e36d9494567eab432fb40d
T0nete <toni.barrantes.montilla64@gmail.com>
2024-03-24 17:59:33 +0100

    remove barnch param

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,9 +1,9 @@
-def call(boolean abortPipeline = false, boolean sonarExecution = true, String branchName = '') {
-    def branch = branchName ?: env.BRANCH_NAME
+def call(boolean abortPipeline = false, boolean sonarExecution = true) {
+    def branch = env.BRANCH_NAME
     
     def shouldAbort = abortPipeline || (branch == 'master') || (branch.startsWith('hotfix'))
 
-    // withSonarQubeEnv('SonarQube') {
+    withSonarQubeEnv('SonarQube') {
         timeout(time: 5, unit: 'MINUTES') {
             if (sonarExecution) {
                 sh 'echo "Ejecución de las pruebas de calidad de código"'
@@ -12,11 +12,10 @@ def call(boolean abortPipeline = false, boolean sonarExecution = true, String br
             }
         }
 
-        if (abortPipeline) {
-            echo 'Evaluando QualityGate de SonarQube...'
+        if (shouldAbort) {
+            error 'Pipeline abortado debido a condiciones de rama.'
         } else {
             echo 'Continuando el pipeline...'
         }
-    // }
+    }
 }
-
